ba769242c5773ece1106f285677e7d46317facc0
zhaoyudong <806094224@qq.com>
2024-05-18 23:02:03 +0800

    基础工程提交

---
+++ src/main/java/com/example/aiserver/AiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aiserver/AiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
